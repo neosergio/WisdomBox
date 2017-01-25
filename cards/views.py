81dcf9c925e11d208d53240d7f1774d3a471fa7d
Sergio Infante <raulsergio9@gmail.com>
2017-01-25 11:20:07 -0500

    Add card detail endpoint

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Card
@@ -11,4 +12,14 @@ class CardList(APIView):
     def get(self, request):
         cards = Card.objects.all()
         serializer = CardSerializer(cards, many=True)
+        return Response(serializer.data)
+
+
+class CardDetail(APIView):
+    def get(self, request, card_id):
+        """
+        Get card details
+        """
+        card = get_object_or_404(Card, pk=card_id)
+        serializer = CardSerializer(card)
         return Response(serializer.data)
--- wisdom/urls.py
+++ wisdom/urls.py
@@ -17,10 +17,11 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from cards.views import CardList
+from cards.views import CardList, CardDetail
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/cards/$', CardList.as_view()),
+    url(r'^api/cards/(?P<card_id>\d+)/$', CardDetail.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
