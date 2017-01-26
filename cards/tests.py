d9cb90bfd60b02ee0e92b36a27a31121bb7b6724
Sergio Infante <raulsergio9@gmail.com>
2017-01-26 11:29:23 -0500

    Change tests to allow unique title field

--- cards/tests.py
+++ cards/tests.py
@@ -6,10 +6,11 @@ from .models import Card
 class CardTestCase(TestCase):
 
     def setUp(self):
-        Card.objects.create(text='foo bar')
-        Card.objects.create(text='bar foo', secondary_text='bar foo 2')
-        Card.objects.create()
+        Card.objects.create(title='card 01', text='foo bar')
+        Card.objects.create(title='card 02', text='bar foo', secondary_text='bar foo 2')
 
     def test_card_creation(self):
         card = Card.objects.get(pk=1)
+        second_card = Card.objects.get(pk=2)
         self.assertEqual(card.text, 'foo bar')
+        self.assertEqual(second_card.secondary_text, 'bar foo 2')
